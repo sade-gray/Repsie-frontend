b74da1fe1023b1bf368c943208eca7943620d95f
MariooC14 <v.caval1@universityofgalway.ie>
2023-10-08 15:24:12 +0100

    Updated DrawerContext syntax to conform with project context standards

--- src/contexts/DrawerContext.tsx
+++ src/contexts/DrawerContext.tsx
@@ -1,7 +1,11 @@
-import {createContext} from "react";
+import {createContext, useContext} from "react";
 import { IContextType } from "../types/contextTypes.ts";
 
 export const DrawerContext = createContext<IContextType>({
     drawerOpen: false,
     setDrawerOpen: () => {},
 });
+
+export function useDrawerContext() {
+    return useContext(DrawerContext);
+}
