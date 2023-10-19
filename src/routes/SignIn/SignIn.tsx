3ea41947557df9c0b6a17dc8c6b02a30f35bbea0
MarioInspiron <v.caval1@universityofgalway.ie>
2023-10-19 14:30:58 +0100

    Added redirect to home from sign up and sign in page if logged in

--- src/routes/SignIn/SignIn.tsx
+++ src/routes/SignIn/SignIn.tsx
@@ -1,12 +1,19 @@
 import { Button } from "@mui/material";
 import SignInForm from "./components/SignInForm.tsx";
+import {useAuth} from "../../contexts/AuthContext.tsx";
+import {Navigate} from "react-router-dom";
 
 export default function SignIn() {
+    const { user }:any = useAuth();
 
-    return (
-        <main className={"sign--in--container"}>
-            <SignInForm />
-            <Button variant={"outlined"} color={"secondary"}>Sign in with Google</Button>
-        </main>
-    )
+    if (user) {
+        return <Navigate to={"/"}/>
+    } else {
+        return (
+            <main className={"sign--in--container"}>
+                <SignInForm/>
+                <Button variant={"outlined"} color={"secondary"}>Sign in with Google</Button>
+            </main>
+        )
+    }
 }
--- src/routes/SignUp/SignUp.tsx
+++ src/routes/SignUp/SignUp.tsx
@@ -1,11 +1,19 @@
 import {Button} from "@mui/material";
 import SignUpForm from "./components/SignUpForm.tsx";
+import {useAuth} from "../../contexts/AuthContext.tsx";
+import {Navigate} from "react-router-dom";
 
 export default function SignUp() {
-    return (
-        <main className={"sign--up--container"}>
-            <SignUpForm/>
-            <Button variant={"outlined"} color={"secondary"}>Sign up with Google</Button>
-        </main>
-    )
+    const {user}:any = useAuth();
+
+    if (user) {
+        return <Navigate to={""} />
+    } else {
+        return (
+            <main className={"sign--up--container"}>
+                <SignUpForm/>
+                <Button variant={"outlined"} color={"secondary"}>Sign up with Google</Button>
+            </main>
+        )
+    }
 }
