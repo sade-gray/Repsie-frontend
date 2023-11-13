28c2a0723861e7a2229c1ef268a5e1b7f7915c39
MarioInspiron <v.caval1@universityofgalway.ie>
2023-11-13 16:59:03 +0000

    MyRecipes page fetches user's created recipe (from firestore). Each recipe display is a link to its page

--- src/routes/MyRecipesPage/MyRecipesPage.tsx
+++ src/routes/MyRecipesPage/MyRecipesPage.tsx
@@ -1,28 +1,59 @@
 import SavedRecipesContainer from "../../components/SavedRecipesContainer";
-import savedRecipes from "../../assets/dummyData/savedRecipes.json";
 import {useEffect, useState} from "react"
 import CreateRecipeButton from "./Components/CreateRecipeButton";
 import { Button, Chip } from "@mui/material";
+import {useAuth} from "../../contexts/AuthContext.tsx";
+import {Link, Navigate} from "react-router-dom";
+import {db, recipesCollectionRef} from "../../firebase.ts";
+import {collection, doc, getDoc, getDocs} from "firebase/firestore";
 
 export function MyRecipesPage() {
-    const [myRecipesComponents, setMyRecipesComponents]: any = useState();
+    const [createdRecipes, setCreatedRecipes]: any = useState([]);
+    const { user } = useAuth();
 
     useEffect(() => {
-        setMyRecipesComponents(savedRecipes.map((recipe: any) => {
-            return (
+        // Only fetch the created recipes if user is logged in
+        if (!user) return;
+        // Get the user's created Recipes
+        const createdRecipesRef = collection(db, "users", user.uid, "createdRecipes");
+        getDocs(createdRecipesRef)
+            .then(async createdRecipesSnap => {
+                if (createdRecipesSnap.empty) {
+                    return;
+                }
+                // For each document in the result
+                createdRecipesSnap.forEach(createdRecipeDocRef => {
+                    // Create a document reference from the doc
+                    const recipeRef = doc(recipesCollectionRef, createdRecipeDocRef.data().recipeRef);
+                    // Fetch the doc and save the data to memory
+                    getDoc(recipeRef).then(doc => {
+                        setCreatedRecipes((prevCreatedRecipes: any[]) => [
+                                ...prevCreatedRecipes,
+                                {
+                                    id: doc.id,
+                                    ...doc.data()
+                                }
+                            ])
+                    }).catch(e => console.log(e));
+                });
+            }).catch(e => console.log(e));
+    }, []) // Get user's created recipes once mounted
+
+    const createdRecipesComponents = createdRecipes.map((recipe: any) => {
+        return (
+            <Link to={`/recipe/${recipe.id}`}  key={recipe.id}>
                 <Button>
-                    <div key={recipe['id']} className="my--recipe--item" style={{backgroundImage: `url(${recipe.imageUrl}`}}>
+                    <div className="my--recipe--item" style={{backgroundImage: `url(${recipe.imageUrl}`}}>
                         <Chip label={recipe.title} variant="filled" color="secondary"/>
                     </div>
                 </Button>
-            )
-        }))
-        setMyRecipesComponents((prevRecipeComponents: Element[]) => {
-            return [...prevRecipeComponents,<CreateRecipeButton key={-1}/>]
-        })
-    }, [])
-
-
+            </Link>
+        )
+    })
+    // Redirect to home if user is not logged in
+    if (!user) {
+        return <Navigate to={"/"} />
+    }
     return (
         <div className={"main--container"}>
             <section className={"saved--recipe--section"}>
@@ -31,7 +62,8 @@ export function MyRecipesPage() {
             <section className={"my--recipes--section"}>
                 <h2>My Recipes</h2>
                 <div className={"my--recipes--container"}>
-                    {myRecipesComponents}
+                    {createdRecipesComponents}
+                    <CreateRecipeButton/>
                 </div>
             </section>
         </div>
