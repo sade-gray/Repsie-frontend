8714967f43e1f46a463c29c89b4c6124ad142be0
Patriks Briedis <patriks.bried@gmail.com>
2024-02-17 19:16:06 +0000

    Added design for page not found

--- src/routes/404Route/RouteNotFound.tsx
+++ src/routes/404Route/RouteNotFound.tsx
@@ -1,3 +1,41 @@
+import { Box, Button, Stack, Typography } from "@mui/material";
+import img from "../../assets/404/pizza-404.png";
+import { ArrowBack } from "@mui/icons-material";
+
 export default function RouteNotFound() {
-  return <div>This route does not exist brother.</div>;
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        width: "100%",
+        flexDirection: "column",
+        gap: "1rem",
+      }}
+    >
+      <Stack
+        direction={"row"}
+        sx={{
+          marginBottom: "2rem",
+        }}
+      >
+        <Stack direction={"column"} alignItems={"center"}>
+          <Typography variant="h3" color={"secondary"}>
+            Page Not Found
+          </Typography>
+          <Typography variant="h6">
+            The page you are looking for does not exist.
+          </Typography>
+        </Stack>
+        <img src={img} alt="logo" width={200} height={200} />
+      </Stack>
+
+      <Button variant="contained" color="secondary" href="/">
+        <ArrowBack />
+        Go Home
+      </Button>
+    </Box>
+  );
 }
