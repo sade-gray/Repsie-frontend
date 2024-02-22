8b0553bb0b9f53b26b152360b0995148ac4cbfc9
PARTH <P.singh6@nuigalway.ie>
2024-02-22 22:56:57 +0000

    basic layout of the profile page

--- src/routes/ProfilePage/ProfilePage.tsx
+++ src/routes/ProfilePage/ProfilePage.tsx
@@ -1,6 +1,60 @@
+import { Email, Password } from '@mui/icons-material';
+import './ProfilePage.css';
+import { useState } from 'react';
+
+//import {useAuth} from '@context/AuthProvider';
+
+
+
 export default function ProfilePage() {
+    const [Birthdate,setBirthdate]= useState("");
+    
 
     return (
-        <h1>page good</h1>
-    )
+        <><div>
+            <input type='file' id='file' name='ProfilePic' accept='image/*' required></input>
+            <label htmlFor='file'>Choose a profile picture</label>
+            <table>
+                <th>Personal Information
+                </th>
+                <tbody>
+                <tr>
+                    <td>Username:</td>
+                    <td><input type="text" id="username" name="username" required></input></td>
+                </tr>
+                <tr>
+                    <td>Birthdate:</td>// adding required
+                    <td>{Birthdate}</td>
+                </tr>
+                <tr>
+                    <td>Email</td>
+                    <td></td> //adding required
+                
+                </tr>
+                <tr>
+                    <td>Password</td>
+                    </tr>
+                    <tr>
+                        <button>Save Changes</button>
+                    </tr>
+                    </tbody>
+            </table>
+        </div>
+         <div>
+            <h2 id="srecipes"> Saved Recipes </h2>
+            
+
+
+            </div>
+            <div>
+                <h3 id="rhistory">Recipe History</h3>
+                </div>            
+            
+            
+            
+            
+            </>
+
+
+);
 }
