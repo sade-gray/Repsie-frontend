202f05272efe6cec24621371b0e0fbbb1c980fd4
mariocaval <cavalmario2003@gmail.com>
2024-02-26 17:32:16 +0000

    Improved docs and styling to the SnackBarContext.tsx component

--- src/contexts/SnackBarProvider/SnackBarContext.tsx
+++ src/contexts/SnackBarProvider/SnackBarContext.tsx
@@ -7,6 +7,10 @@ import { snackContextValues } from "./snackTypes";
 
 export const SnackBarContext = createContext({} as snackContextValues);
 
+/**
+ * Provides the app with an API that lets you add a small notification to the bottom right of the screen
+ * @param children the app to wrap
+ */
 export function SnackBarProvider({ children }: { children: ReactNode }) {
   const [snack, setSnack] = useState<snack>({
     message: "",
@@ -15,18 +19,16 @@ export function SnackBarProvider({ children }: { children: ReactNode }) {
   const [open, setOpen] = useState<boolean>(false);
 
   /**
-   * Adds a snack to the snack bar on the bottom right of the screen
-   * @param message
-   * @param severity
+   * Adds a snack to the bottom right of the screen
+   * @param message the message to show
+   * @param severity the type of notification (green, yellow, red etc...) See material ui for more details
    */
   const addSnack = (message: string, severity?: AlertColor) => {
     setSnack({ message: message, severity: severity || "success" });
     setOpen(true);
   };
 
-  const value: snackContextValues = {
-    addSnack,
-  };
+  const value: snackContextValues = {addSnack};
 
   const handleClose = () => {
     setOpen(false);
@@ -34,11 +36,8 @@ export function SnackBarProvider({ children }: { children: ReactNode }) {
 
   return (
     <SnackBarContext.Provider value={value}>
-      <Snackbar
-        open={open}
-        autoHideDuration={5000}
-        onClose={handleClose}
-        anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+      <Snackbar open={open} autoHideDuration={5000} onClose={handleClose}
+                anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
       >
         <Alert severity={snack.severity} onClose={handleClose}>
           {snack.message}
