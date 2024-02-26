363a5c25e768c9acac6f3fa3f529a3471cb5d34f
mariocaval <cavalmario2003@gmail.com>
2024-02-26 17:27:28 +0000

    Improved docs on DrawerProvider

--- src/contexts/DrawerProvider/DrawerContext.tsx
+++ src/contexts/DrawerProvider/DrawerContext.tsx
@@ -1,11 +1,16 @@
 import { createContext, ReactNode, useState } from "react";
 import { IContextType } from "../../types/contextTypes";
 
+/** The context values to export as the API*/
 export const DrawerContext = createContext<IContextType>({
   drawerOpen: false,
   setDrawerOpen: () => {},
 });
 
+/**
+ * Provides the app with the ability to toggle the saved recipes drawer on or off
+ * @param children the app to wrap
+ */
 export function DrawerProvider({ children }: { children: ReactNode }) {
   const [drawerOpen, setDrawerOpen] = useState<boolean>(false);
 
