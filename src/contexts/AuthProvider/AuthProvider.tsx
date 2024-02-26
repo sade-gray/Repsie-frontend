1e55cc68a4c847553c11944203448cad1cd5c4e0
mariocaval <cavalmario2003@gmail.com>
2024-02-26 17:24:00 +0000

    Improved documentation on the AuthProvider.tsx component

--- src/contexts/AuthProvider/AuthProvider.tsx
+++ src/contexts/AuthProvider/AuthProvider.tsx
@@ -1,28 +1,34 @@
-import { createContext, ReactNode, useEffect, useState } from "react";
-import { auth } from "../../firebase.ts";
-import { User as FirebaseUser } from "firebase/auth";
+import { createContext, ReactNode, useEffect, useState } from 'react';
+import { auth } from '../../firebase.ts';
 import {
   createUserWithEmailAndPassword,
   onAuthStateChanged,
   signInWithEmailAndPassword,
-} from "firebase/auth";
-import useSnackBar from "@context/SnackBarProvider";
-import { useNavigate } from "react-router-dom";
-import { AuthContextValues } from "./authTypes";
+  User as FirebaseUser,
+} from 'firebase/auth';
+import useSnackBar from '@context/SnackBarProvider';
+import { useNavigate } from 'react-router-dom';
+import { AuthContextValues } from './authTypes';
 
 export const AuthContext = createContext({} as AuthContextValues);
 
+/**
+ * Provides the app with authentication functionality
+ * @param children the app to wrap
+ */
 export function AuthProvider({ children }: { children: ReactNode }) {
-  const [user, setUser] = useState<FirebaseUser | null | undefined>();
+  const [user, setUser] = useState<FirebaseUser | null>();
   const [loading, setLoading] = useState(true);
   const { addSnack } = useSnackBar();
   const navigate = useNavigate();
 
-  const emailSignUp = async (
-    email: string,
-    password: string
-  ): Promise<boolean> => {
-    await createUserWithEmailAndPassword(auth, email, password)
+  /**
+   * Signs user up with email and password.
+   * @param email
+   * @param password
+   */
+  const emailSignUp = async (email: string, password: string) => {
+    return await createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         const user = userCredential.user;
         if (user) {
@@ -33,22 +39,25 @@ export function AuthProvider({ children }: { children: ReactNode }) {
         return false;
       })
       .catch((error) => {
-        console.log(error.message);
+        console.error(error.message);
         return false;
       });
-    return false;
   };
-  const emailSignIn = async (
-    email: string,
-    password: string
-  ): Promise<boolean> => {
-    await signInWithEmailAndPassword(auth, email, password)
+
+  /**
+   * Signs user up with email and password
+   * @param email
+   * @param password
+   * @return whether the user was able to sign in
+   */
+  const emailSignIn = async (email: string, password: string) => {
+    return await signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         const user = userCredential.user;
         if (user) {
           setUser(user);
           addSnack(`Success! Logged in as ${user.displayName || user.email}`);
-          navigate("/");
+          navigate('/');
           return true;
         }
         return false;
@@ -57,14 +66,19 @@ export function AuthProvider({ children }: { children: ReactNode }) {
         console.log(error.message);
         return false;
       });
-    return false;
   };
+
+  /**
+   * Signs the user out
+   */
   const signOut = () => {
     return auth.signOut();
   };
   // TODO: Add update email and password functions
   // TODO: Add Google Auth.
 
+  // Adds an event listener to the user's authentication status
+  // E.g. Makes the user object null when signed out.
   useEffect(() => {
     return onAuthStateChanged(auth, (user) => {
       setUser(user);
@@ -72,6 +86,7 @@ export function AuthProvider({ children }: { children: ReactNode }) {
     });
   }, []);
 
+  // The values this Provider provides for its API.
   const value: AuthContextValues = {
     user,
     emailSignUp,
