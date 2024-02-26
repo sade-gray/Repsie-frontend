9f73af35f0757bd2f133bb317bdbdf2451c1d942
mariocaval <cavalmario2003@gmail.com>
2024-02-26 17:32:53 +0000

    Removed addSnack doc from type declaration

--- src/contexts/SnackBarProvider/snackTypes.d.ts
+++ src/contexts/SnackBarProvider/snackTypes.d.ts
@@ -1,11 +1,6 @@
 import { AlertColor } from "@mui/material/Alert/Alert";
 
 export interface snackContextValues {
-  /**
-   * Adds a snack to the bottom right of the screen
-   * @param message
-   * @param severity
-   */
   addSnack: (message: string, severity?: AlertColor | undefined) => void;
 }
 
