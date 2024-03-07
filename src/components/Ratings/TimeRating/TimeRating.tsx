f79ed7c35df4a4259dfdbf5d31b8b3466b728961
mariocaval <cavalmario2003@gmail.com>
2024-03-07 14:54:16 +0000

    Added size customizability for Rating components part 2

--- src/components/Ratings/TimeRating/TimeRating.tsx
+++ src/components/Ratings/TimeRating/TimeRating.tsx
@@ -2,7 +2,7 @@ import { ratingProps } from '../ratingClasses';
 import { AccessTime } from '@mui/icons-material';
 import { Rating, styled } from '@mui/material';
 
-function TimeRating({ value, readOnly, handleChange }: ratingProps) {
+function TimeRating({ value, readOnly, handleChange, size = 'large' }: ratingProps) {
   const StyledRating = styled(Rating)(({ theme }) => ({
     '& .MuiRating-iconEmpty .MuiSvgIcon-root': {
       color: theme.palette.action.disabled,
