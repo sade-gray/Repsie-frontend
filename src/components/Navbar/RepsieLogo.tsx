09cf806b31274a749e68c118f7a3a69b78dfe71a
mariocaval <cavalmario2003@gmail.com>
2024-03-25 15:35:49 +0000

    Improved style and responsiveness of header logo and bookmark icon

--- src/components/Navbar/RepsieLogo.tsx
+++ src/components/Navbar/RepsieLogo.tsx
@@ -1,18 +1,11 @@
 import { useMediaQuery } from '@mui/material';
 import { useTheme } from '@mui/material';
 import fullLogo from '../../assets/logo.svg';
-import shortLogo from '/favicon.svg?url';
-import { Link } from 'react-router-dom';
+import shortLogo from '/favicon.svg';
+import { useNavigate } from 'react-router-dom';
 
 export default function RepsieLogo() {
   const theme = useTheme();
-  return (
-    <Link to={'/'}>
-      <img
-        className={'repsie--logo'}
-        src={useMediaQuery(theme.breakpoints.up('sm')) ? fullLogo : shortLogo}
-        alt={'Repsie logo'}
-      />
-    </Link>
-  );
+  const navigate = useNavigate();
+  return <img onClick={() => navigate('/')} src={useMediaQuery(theme.breakpoints.up('md')) ? fullLogo : shortLogo} alt={'Repsie logo'} />;
 }
